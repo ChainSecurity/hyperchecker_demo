bf40cd7ee3f420d58c81e3132b59e1080c366ba0
tobias <kaisert@ethz.ch>
2018-03-27 16:03:56 +0200

    asdf

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/hyperledger/fabric/core/chaincode/shim"
-  "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
 )
 
 type SimpleAsset struct {
 }
 
 func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
-  return shim.Success(nil)
+	return shim.Success([]byte("success"))
 }
 
 func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-  _, args := stub.GetFunctionAndParameters()
-  if len(args) == 0 {
-     return shim.Error("No argument specified.")
-  }
-  //second return value stands for potential error
-  result, _ := stub.GetState(args[0])
-
-  return shim.Success(result)
+    tByte, err := GetTimeByteArray()
+    if err != nil {
+        return shim.Error(err.Error())
+    }
+    err = stub.PutState("key", tByte)
+    if err != nil {
+        return shim.Error(err.Error())
+    }
+	return shim.Success([]byte("success"))
 }
 
 func main() {
-  if err := shim.Start(new(SimpleAsset)); err != nil {
-     fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
-  }
+	if err := shim.Start(new(SimpleAsset)); err != nil {
+		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+	}
 }
