c372205e0f4c9eaef59f2cabf94e37fd5079d436
tobias <kaisert@ethz.ch>
2018-03-27 15:30:51 +0200

    asdf

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/protos/peer"
+  "fmt"
+
+  "github.com/hyperledger/fabric/core/chaincode/shim"
+  "github.com/hyperledger/fabric/protos/peer"
 )
 
 type SimpleAsset struct {
 }
 
-func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
-	return shim.Success(nil)
+type simpleStruct struct {
+    key string
+    data string
 }
 
-func (t SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-	returnValue := 0
-	myMap := make(map[int]int)
-	for i, ii := range myMap {
-		returnValue = returnValue * i - ii
-	}
-	return shim.Success([]byte("value: " + string(returnValue)))
+type simpleStruct2 struct {
+    ss simpleStruct
 }
+
+func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+  key := "testKey"
+  data := "dataInit"
+  err := stub.PutState(key, []byte(data))
+  if err != nil {
+     return shim.Error(err.Error())
+  }
+  return shim.Success(nil)
+}
+
+func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+  keyValuePair := simpleStruct {
+      key: "testKey",
+      data: "dataNew",
+  }
+
+  ss2 := simpleStruct2 {
+      ss: keyValuePair,
+  }
+
+  err := stub.PutState(ss2.ss.key, []byte(keyValuePair.data))
+  if err != nil {
+     return shim.Error("could not write new data")
+  }
+  //vulnerability
+  respone, err := stub.GetState(ss2.ss.key)
+  if err != nil {
+     return shim.Error("could not read data")
+  }
+
+  return shim.Success([]byte(respone))
+}
+
 
 func main() {
-	if err := shim.Start(new(SimpleAsset)); err != nil {
-		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
-	}
+  if err := shim.Start(new(SimpleAsset)); err != nil {
+     fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+  }
 }
