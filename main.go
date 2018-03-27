90863572799ff409bc8fa39565247cc3adef17e0
tobias <kaisert@ethz.ch>
2018-03-27 15:14:20 +0200

    asdf

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/protos/peer"
+  "fmt"
+  "github.com/hyperledger/fabric/core/chaincode/shim"
+  "github.com/hyperledger/fabric/protos/peer"
 )
 
 type SimpleAsset struct {
+    //Vulnerability
+    globalValue string
 }
 
 func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
-	return shim.Success([]byte("success"))
+  return shim.Success(nil)
 }
 
-func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-    tByte, err := GetTimeByteArray()
-    if err != nil {
-        return shim.Error(err.Error())
-    }
-    err = stub.PutState("key", tByte)
-    if err != nil {
-        return shim.Error(err.Error())
-    }
-	return shim.Success([]byte("success"))
+func (t SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+  fn, args := stub.GetFunctionAndParameters()
+
+  if fn == "setValue" {
+     t.globalValue = args[0]
+     return shim.Success([]byte(t.globalValue))
+  } else if fn == "getValue" { // assume 'get' even if fn is nil
+      myValue := t.globalValue
+     return shim.Success([]byte(myValue))
+  }
+  return shim.Error("not a valid function")
 }
 
 func main() {
-	if err := shim.Start(new(SimpleAsset)); err != nil {
-		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
-	}
+  if err := shim.Start(new(SimpleAsset)); err != nil {
+     fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+  }
 }
