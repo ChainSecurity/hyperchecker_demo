e888c71053fca26944421e17453ea3752d1dced7
tobias <kaisert@ethz.ch>
2018-03-28 18:52:49 +0200

    asdf

--- main.go
+++ main.go
@@ -11,19 +11,34 @@ type SimpleAsset struct {
 }
 
 func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
-	return shim.Success([]byte("success"))
+	key := "testKey"
+	data := "dataInit"
+	err := stub.PutState(key, []byte(data))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(nil)
 }
 
 func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-    tByte, err := GetTimeByteArray()
+	key1 := "testKey1"
+
+    err := writeToLedger(stub, &key1)
     if err != nil {
-        return shim.Error(err.Error())
+        return shim.Error("could not write data")
     }
-    err = stub.PutState("key", tByte)
-    if err != nil {
-        return shim.Error(err.Error())
-    }
-	return shim.Success([]byte("success"))
+
+	//vulnerability
+	respone, err := stub.GetState(key1)
+	if err != nil {
+		return shim.Error("could not read data")
+	}
+
+	return shim.Success([]byte(respone))
+}
+
+func writeToLedger(stub shim.ChaincodeStubInterface, key *string) error {
+	return stub.PutState(*key, []byte("data"))
 }
 
 func main() {
